4c888fcb1f6847d026d6ad2cc76546ddb2ca22ea
Will Arends <willarends@gmail.com>
2016-08-15 10:08:09 -0600

    chaching post data

--- public/app/blog/postDetailCtrl.js
+++ public/app/blog/postDetailCtrl.js
@@ -1,3 +1,10 @@
-angular.module('willsBlog').controller('postDetailCtrl', function($scope, mvPost, $routeParams){
-  $scope.post = mvPost.get({ _id: $routeParams.id });
+angular.module('willsBlog').controller('postDetailCtrl', function($scope, mvCachedPost, $routeParams){
+  mvCachedPost.query().$promise.then(function(collection){
+    collection.forEach(function(post){
+      if(post._id === $routeParams.id){
+        $scope.post = post;
+      }
+    });
+  });
+  // $scope.post = mvPost.get({ _id: $routeParams.id });
 });
