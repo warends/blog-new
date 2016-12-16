39cc6bfc290e167d8deebc3dcdc1041cbdeca58b
Will Arends <willarends@gmail.com>
2016-12-16 13:54:27 -0700

    Revert "Smooth scroll"

--- public/app/main/navCtrl.js
+++ public/app/main/navCtrl.js
@@ -1,28 +1,7 @@
-angular.module('willsBlog').controller('navCtrl', ['$scope', '$location', '$anchorScroll', '$document', function($scope, $location, $anchorScroll, $document){
-
-  $scope.scrollTop = function(){
-    $document.scrollTopAnimated(0);
-  }
+angular.module('willsBlog').controller('navCtrl', ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll){
   $scope.linkTo = function(id){
-
-      var route = $location.url();
-      var offset = 60;
-      var duration = 800;
-      var element = angular.element(id);
-
-      if(route !== '/'){
-        //console.log('another route + ' + route);
-        $location.url('/', function(){
-          $document.scrollToElement(element, offset, duration);
-        });
-
-      } else {
-        //console.log('same route');
-        $document.scrollToElement(element, offset, duration);
-      }
-
-     //$location.url(id);
-     //$anchorScroll();
+    $location.url(id);
+    $anchorScroll();
   };
 
 }]);
