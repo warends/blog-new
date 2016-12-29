dfa02cf516137728b5e77ab56ea0be6c23951af5
Will Arends <willarends@gmail.com>
2016-12-29 09:17:51 -0800

    workCtrl ng-repeat

--- public/app/main/workCtrl.js
+++ public/app/main/workCtrl.js
@@ -1,23 +1,50 @@
 angular.module('willsBlog').controller('workCtrl', ['$scope', function($scope){
 
-  $scope.tendrilShown = false;
-  $scope.toggleTendril = function() {
-    $scope.tendrilShown = !$scope.tendrilShown;
-  };
-
-  $scope.crownShow = false;
-  $scope.toggleCrown = function() {
-    $scope.crownShow = !$scope.crownShow;
-  };
-
-  $scope.broadShow = false;
-  $scope.toggleBroad = function() {
-    $scope.broadShow = !$scope.broadShow;
-  };
-
-  $scope.adihow = false;
-  $scope.toggleAdi = function() {
-    $scope.adiShow = !$scope.adiShow;
-  };
+  $scope.projects = [
+    {
+      name: 'Tendril Inc.',
+      link: 'https://www.tendrilinc.com',
+      clickFunc: toggleTendril = function(){
+        this.modal = !this.modal;
+      },
+      backgroundImg: 'img/gallery/tendril.png',
+      modal: false,
+      modalImg: 'img/gallery/tendril-mockup.png',
+      noLink: false
+    },
+    {
+      name: 'Crown Offroad',
+      link: 'http://crownoffroad.com',
+      clickFunc: toggleCrown = function() {
+        this.modal = !this.modal;
+      },
+      backgroundImg: 'img/gallery/crown-offroad.jpg',
+      modal: false,
+      modalImg: 'img/gallery/crown-offroad-mockup.jpg',
+      noLink: false
+    },
+    {
+      name: 'Colorado Broadband Portal',
+      link: 'http://broadband.co.gov',
+      clickFunc: $scope.toggleBroad = function() {
+        this.modal = !this.modal;
+      },
+      backgroundImg: 'img/gallery/wires.jpg',
+      modal: false,
+      modalImg: 'img/gallery/broadband.jpg',
+      noLink: false
+    },
+    {
+      name: 'adidas adiPlayer',
+      link: '',
+      clickFunc: toggleAdi = function() {
+        this.modal = !this.modal;
+      },
+      backgroundImg: 'img/gallery/adiplayer.jpg',
+      modal: false,
+      modalImg: 'img/gallery/adiplayer_mockup.jpg',
+      noLink: true
+    }
+  ]
 
 }]);
