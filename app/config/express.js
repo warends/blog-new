8cbf68148472784f67e99f25d6ccfba744047497
Will Arends <willarends@gmail.com>
2017-01-01 10:45:13 -0800

    express compression

--- app/config/express.js
+++ app/config/express.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     logger = require('morgan'),
+    h5bp = require('h5bp'),
     bodyParser = require('body-parser'),
     engine = require('ejs-locals'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
+    compression = require('compression'),
     passport = require('passport');
 
 module.exports = function(app, config){
@@ -25,5 +27,9 @@ module.exports = function(app, config){
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(express.static(config.rootPath + '/public'));
+  app.use(compression());
+  // app.use(h5bp({ root: __dirname + '/public' }));
+  // app.use(compression());
+  // app.use(express.static(config.rootPath + '/public'));
 
 }
