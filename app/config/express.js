4fe46706b0f8c1d34120600ccb93c0c044fb52e4
Will Arends <willarends@gmail.com>
2017-01-01 10:50:57 -0800

    removed h5bp

--- app/config/express.js
+++ app/config/express.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     logger = require('morgan'),
-    h5bp = require('h5bp'),
     bodyParser = require('body-parser'),
     engine = require('ejs-locals'),
     cookieParser = require('cookie-parser'),
@@ -28,8 +27,5 @@ module.exports = function(app, config){
   app.use(passport.session());
   app.use(express.static(config.rootPath + '/public'));
   app.use(compression());
-  // app.use(h5bp({ root: __dirname + '/public' }));
-  // app.use(compression());
-  // app.use(express.static(config.rootPath + '/public'));
 
 }
